5d0fe1a3654aa06ae66231faee3f839079a91261
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2022-08-12 15:27:17 -0300

    Commit "Teste 1" - Criando nova tela
    Criando a tela de cadastro de EPI´S

--- codes/script.js
+++ codes/script.js
@@ -30,3 +30,17 @@ for (var i = 0 ; i < inputs.length; i++) {
       }
    })
 }
+
+function mais(){
+    var atual = document.getElementById("total").value;
+    var novo = atual - (-1); //Evitando Concatenacoes
+    document.getElementById("total").value = novo;
+  }
+  
+  function menos(){
+    var atual = document.getElementById("total").value;
+    if(atual > 0) { //evita números negativos
+      var novo = atual - 1;
+      document.getElementById("total").value = novo;
+    }
+  }
